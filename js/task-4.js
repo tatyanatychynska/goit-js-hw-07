5fe8d51cbce91d2b62abfdec7e892fca6fd7dbab
Tanya Tychynska <tatyanazyeva@gmail.com>
2026-02-25 01:42:54 +0100

    HW07 t4,5

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,20 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    
+    const emailVal = form.elements.email.value.trim();
+    const passwordVal = form.elements.password.value.trim();
+    if (emailVal ==='' || passwordVal === '') {
+        alert('All form fields must be filled in');
+        return;
+    } 
+        const obj = {
+            email: emailVal,
+            password: passwordVal,
+        };
+
+        console.log(obj);
+    form.reset();
+})
--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,17 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const btnClick = document.querySelector('.change-color');
+btnClick.addEventListener('click', changeColor);
+
+const spanColor = document.querySelector('.color');
+const bodyColor = document.querySelector('body');
+
+function changeColor() {
+  const color = getRandomHexColor();
+  
+  bodyColor.style.backgroundColor = color;
+  spanColor.textContent = color;
+  return;
+}
